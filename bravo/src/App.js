91a7fb9272303d9fb400c92eb1403ed79e8d8a03
Administrator <lumengbit2@gmail.com>
2019-01-25 23:55:08 +1100

    first commit

---
+++ bravo/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>Bravo</p>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
